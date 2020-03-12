cc3802078d886d0dd92ef4125dea743d25272ab1
Sebastiaan Beekman <0963401@hr.nl>
2020-03-12 11:09:50 +0100

    Code, FAT working

--- SeboOSV3/SeboOSV3.ino
+++ SeboOSV3/SeboOSV3.ino
@@ -1,9 +1,63 @@
+#include <EEPROM.h>
+
+typedef struct {
+  char Name[12] = {};
+  int Size;
+  int beginPos;
+} FAT;
+
+EERef noOfFiles = EEPROM[160];
+int sizeOfFAT = sizeof(FAT);
+
 void setup() {
-  // put your setup code here, to run once:
+  Serial.begin(9600);
+#ifdef DEBUG
+  Serial.println("Welcome to SeboOS V0.1, Debug mode.");
+#else
+  Serial.println("Welcome to SeboOS V0.1.");
+#endif
+}
+
+bool writeFATEntry(char Name[], int Size) {                    //1.Check name 2. Check space 3. Profit
+  int adress = checkFATSize();
+  if (adress == -1) {
+    Serial.println(There is no space in the FAT...);
+    return false;
+  } else if (checkFATDuplicates(Name)) {
+    Serial.println("This filename is already in use...");
+    return false;
+  } else if () {
+
+  }
 
 }
 
+int checkFATSize() {
+  int temp;
+  for (int i = 0; i < 10; i++) {
+    EEPROM.get(i * sizeOfFAT, temp);
+    if (temp == -1) return i * sizeOfFAT;                     //return adress
+  }
+  return -1;
+}
+
+bool checkFATDuplicates(char Name[]) {
+  FAT FATEntry;
+  for (int i = 0; i < noOfFiles; i++) {
+    EEPROM.get(i * sizeOfFAT, FATEntry);
+    if (strncmp(Name, FATEntry.Name, 12) == 0) return true;
+  }
+  return false;
+}
+
+int checkAllocationSpace(byte Size) {
+  
+}
+
+FAT* sortFAT() {
+  
+}
+
 void loop() {
-  // put your main code here, to run repeatedly:
 
 }
--- Test/Test.ino
+++ Test/Test.ino
@@ -1,31 +1,36 @@
-#define BUFFERSIZE 15
-static bool flag = true;
-char* p;
+#include <EEPROM.h>
+
+static int FATaddr = 0;
+static int EEPROMaddr = 0;
+
+typedef struct {
+  char Name[12];
+  int beginPos;
+  int Size;
+} FAT;
+
+static FAT FATArray[32];
+
+void addFATEntry(FAT file) {
+  FATArray[FATaddr++] = file;
+}
+
+void deleteFATEntry(int pos) {
+  
+}
+
+FAT writeFATEntry(char[] Name, int beginPos, int Size) {
+  
+}
+
+FAT readFATEntry() {
+  
+}
 
 void setup() {
   Serial.begin(9600);
-  p = returnArray();
-}
-
-char* returnArray() {
-  static char testArray[BUFFERSIZE] = "sebastiaan";
-  return testArray;
-}
-
-char* returnArray2() {
-  static char testArray[BUFFERSIZE] = "frans";
-  return testArray;
-}
-
-void clearBuffer(char* p) {
-  memset(p, 0, BUFFERSIZE);
 }
 
 void loop() {
-  for (int i = 0; i < BUFFERSIZE; i++) {
-    if (*(p+i)!= NULL) {
-    Serial.print(*(p + i));
-    }
-  }
-  clearBuffer(p);
+    
 }
